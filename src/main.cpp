4316b47de3742d7125d249e865ba2a9cce232eed
真夜中の雰囲気午夜氛圍 <98058348+midnightvibe@users.noreply.github.com>
2023-09-22 03:58:43 +0200

    refactored

--- src/main.cpp
+++ src/main.cpp
@@ -2,85 +2,78 @@
 #include <XboxSeriesXControllerESP32_asukiaaa.hpp>
 #include <ESP32Servo.h>
 
-// Required to replace with your xbox address
-// XboxSeriesXControllerESP32_asukiaaa::Core
-// xboxController("44:16:22:5e:b2:d4");
-
-// any xbox controller
 XboxSeriesXControllerESP32_asukiaaa::Core xboxController;
 Servo K969Servo;
-int servoPin = 18;
+
+const int servoPin = 18;
+const float MAX_STEERING_ANGLE = 160.0;
+const float MIN_STEERING_ANGLE = 20.0;
+const float CENTER_STEERING_ANGLE = 90.0;
+const float CENTER_TOLERANCE = 3.0;
+
 int flag = 0;
-float steeringAngle = 0.00;
 
 void setup() {
     Serial.begin(115200);
     Serial.println("Starting NimBLE Client");
     xboxController.begin();
     K969Servo.attach(servoPin);
-    K969Servo.write(90);
+    K969Servo.write(CENTER_STEERING_ANGLE);
 }
 
 void demoVibration() {
     XboxSeriesXHIDReportBuilder_asukiaaa::ReportBase repo;
-
     repo.v.select.center = true;
-    repo.v.select.left = false;
-    repo.v.select.right = false;
-    repo.v.select.shake = false;
     repo.v.power.center = 80;  // 60% power
     repo.v.timeActive = 35;    // 0.35 second
-    Serial.println("\nController Connected Succesfully");
+    Serial.println("\\nController Connected Successfully");
     xboxController.writeHIDReport(repo);
+}
+
+float calculateSteeringAngle(float joyLHoriValue) {
+    float steeringAngle = (joyLHoriValue - 65535.0) / 65535.0 * (-180);
+    
+    if (steeringAngle <= MIN_STEERING_ANGLE) {
+        return MIN_STEERING_ANGLE;
+    } else if (steeringAngle >= MAX_STEERING_ANGLE) {
+        return MAX_STEERING_ANGLE;
+    } else if (abs(steeringAngle - CENTER_STEERING_ANGLE) <= CENTER_TOLERANCE) {
+        return CENTER_STEERING_ANGLE;
+    } else {
+        return steeringAngle;
+    }
 }
 
 void loop() {
     xboxController.onLoop();
     if (xboxController.isConnected()) {
         if (xboxController.isWaitingForFirstNotification()) {
-        Serial.println("waiting for first notification");
+            Serial.println("waiting for first notification");
         } else {
             if (flag == 0) {
                 demoVibration();
                 Serial.println("Address: " + xboxController.buildDeviceAddressStr());
                 Serial.println("battery " + String(xboxController.battery) + "%");
-                flag +=1;
+                flag += 1;
             }
-            //uint16_t joystickMax = XboxControllerNotificationParser::maxJoy;
+            
+            float joyLHoriValue = (float)xboxController.xboxNotif.joyLHori;
+            float steeringAngle = calculateSteeringAngle(joyLHoriValue);
+            K969Servo.write(steeringAngle);
+            
             Serial.print("trigRT value: ");
             Serial.print((float)xboxController.xboxNotif.trigRT);
-            Serial.print("\ttrigLT value: ");
+            Serial.print("\\ttrigLT value: ");
             Serial.print((float)xboxController.xboxNotif.trigLT);
-            Serial.print("\tjoyLHori rate: ");
-            Serial.print((float)xboxController.xboxNotif.joyLHori);
-
-            // calculate and output steering angle from joyLHori Input
-            steeringAngle = (((float)(xboxController.xboxNotif.joyLHori)-(float)(65535))/((float)(65535))*(-180)); // -35° da sonst 
-            if (steeringAngle <= 20) {
-                steeringAngle = 20;
-                K969Servo.write(steeringAngle);
-                Serial.print("\t    steering angle: ");
-                Serial.println((float)steeringAngle);
-            } else if (steeringAngle >= 160) {
-                steeringAngle = 160;
-                K969Servo.write(steeringAngle);
-                Serial.print("\t    steering angle: ");
-                Serial.println((float)steeringAngle);
-            } else if (steeringAngle >= 87 && steeringAngle <= 93 ) {
-                steeringAngle = 90;
-                K969Servo.write(steeringAngle);
-                Serial.print("\t    steering angle: ");
-                Serial.println((float)steeringAngle);
-            } else {
-                K969Servo.write(steeringAngle);
-                Serial.print("\t    steering angle: ");
-                Serial.println((float)steeringAngle);
-            }
+            Serial.print("\\tjoyLHori rate: ");
+            Serial.print(joyLHoriValue);
+            Serial.print("\\tsteering angle: ");
+            Serial.println(steeringAngle);
         }
     } else {
         Serial.println("not connected");
         if (xboxController.getCountFailedConnection() > 2) {
-        ESP.restart();
+            ESP.restart();
         }
     }
 }
